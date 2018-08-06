0dbe100f6d76573763ad387b06b3a9d58503075c
lishiwei <15240275367@163.com>
2018-08-06 20:17:34 +0800

    同步组件AQS

---
+++ src/main/java/cn/lishiwei/learn/lesson_8_Thread/lesson_8_4_ReentranLock/main.java
@@ -0,0 +1,21 @@
+package cn.lishiwei.learn.lesson_8_Thread.lesson_8_4_ReentranLock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author: LiShiwei.
+ * @Date:Created in 2018/8/6.
+ * @Description:
+ * @Modified By:
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+        //参数用于控制创建公平锁还是非公平锁
+        Lock lock = new ReentrantLock();
+
+    }
+
+}
